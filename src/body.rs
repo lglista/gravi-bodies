e5921460b86c53cae01c65e7e4eebfc70f13c1d5
Lawrence Glista <lawrenceglista@gmail.com>
2023-07-16 21:47:32 -0400

    MVP without collisions

--- src/body.rs
+++ src/body.rs
@@ -1,10 +1,12 @@
 use sfml::system::Vector2f;
 
+static G: f32 = 6.674 * 0.001; // 6.6764E-3 (6.6764E-11 is normal, will be added to config file)
+
 // The laws of physics dictate that for every action there is an opposite and equal reaction.
 // i.e. If one body type pushes, the other has to push as well
 // This makes it so that there can only be 1 universal body type meaning that only 1 at a time can have absolute reign
-// Here it is chosen to be WEIRD. This makes it so that even NEUTRAL is repelled by WEIRD
-#[derive(PartialEq, Eq)]
+// It is chosen in lib::body_type_multiplier() to be WEIRD. This makes it so that even NEUTRAL is repelled by WEIRD
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub enum BodyType {
     POSITIVE,
     NEGATIVE,
@@ -14,16 +16,134 @@ pub enum BodyType {
 
 pub struct Body {
     pub velocity: Vector2f,
-    pub position: Vector2f,
     pub mass: f32,
     pub body_type: BodyType
 }
 
 impl Body {
-    pub fn new(starting_velocity: Vector2f,
-                starting_position: Vector2f,
-                starting_mass: f32,
-                starting_body_type: BodyType) -> Self {
-        Body{velocity: starting_velocity, position: starting_position, mass: starting_mass, body_type: starting_body_type}
+    pub fn new(starting_velocity: Vector2f, starting_mass: f32,starting_body_type: BodyType) -> Self {
+        Body{velocity: starting_velocity, mass: starting_mass, body_type: starting_body_type}
     }
+}
+
+pub fn length_of_vector(vec: Vector2f) -> f32 {
+    let x_2: f32 = vec.x * vec.x;
+    let y_2: f32 = vec.y * vec.y;
+    f32::sqrt(x_2 + y_2)
+}
+
+pub fn normalize(vec: Vector2f, length: f32) -> Vector2f {
+    Vector2f::new(vec.x / length, vec.y / length)
+}
+
+pub fn calculate_force_of_gravity(m1: f32, m2: f32, r: f32) -> f32 {
+    let numerator: f32 = m1 * m2;
+    let denominator: f32 = r.powf(2_f32);
+    G * (numerator / denominator)
+}
+
+pub fn body_type_multiplier(a: BodyType, b: BodyType) -> i32 {
+    if a == BodyType::WEIRD || b == BodyType::WEIRD {
+        return -1;
+    }
+    else if a == BodyType::NEUTRAL || b == BodyType::NEUTRAL {
+        return 1;
+    }
+    else if a == b {
+        return -1;
+    }
+    else {
+        return 1;
+    }
+}
+
+
+/*
+ * ###############################################################################
+ * ################################# TESTS #######################################
+ * ###############################################################################
+ */
+#[test]
+fn length_of_vector_basic() {
+    let vec = Vector2f::new(0_f32, 1_f32);
+    assert_eq!(length_of_vector(vec), 1_f32);
+}
+
+#[test]
+fn length_of_vector_perfect_square() {
+    let vec_1 = Vector2f::new(3_f32, 4_f32);
+    assert_eq!(length_of_vector(vec_1), 5_f32);
+
+    let vec_2 = Vector2f::new(5_f32, 12_f32);
+    assert_eq!(length_of_vector(vec_2), 13_f32);
+}
+
+#[test]
+fn normalize_basic() {
+    let vec_1 = Vector2f::new(0_f32, 1_f32);
+    let normalized_1 = normalize(vec_1, 1_f32);
+    assert_eq!(normalized_1.x, 0_f32);
+    assert_eq!(normalized_1.y, 1_f32);
+
+    let vec_2 = Vector2f::new(0_f32, 120_f32);
+    let normalized_2 = normalize(vec_2, 120_f32);
+    assert_eq!(normalized_2.x, 0_f32);
+    assert_eq!(normalized_2.y, 1_f32);
+}
+
+#[test]
+fn normalize_square() {
+    let vec = Vector2f::new(3_f32, 4_f32);
+    let normalized = normalize(vec, 5_f32);
+    assert_eq!(normalized.x, 0.6_f32);
+    assert_eq!(normalized.y, 0.8_f32);
+}
+
+#[test]
+fn normalize_negative() {
+    let vec = Vector2f::new(-3_f32, 4_f32);
+    let normalized = normalize(vec, 5_f32);
+    assert_eq!(normalized.x, -0.6_f32);
+    assert_eq!(normalized.y, 0.8_f32);
+}
+
+#[test]
+fn integrate_normalize_with_length_basic() {
+    let vec = Vector2f::new(-3_f32, 4_f32);
+    let normalized = normalize(vec, length_of_vector(vec));
+    assert_eq!(normalized.x, -0.6_f32);
+    assert_eq!(normalized.y, 0.8_f32);
+}
+
+#[test]
+fn body_type_multiplier_positive() {
+    assert_eq!(body_type_multiplier(BodyType::POSITIVE, BodyType::POSITIVE), -1);
+    assert_eq!(body_type_multiplier(BodyType::POSITIVE, BodyType::NEGATIVE), 1);
+    assert_eq!(body_type_multiplier(BodyType::POSITIVE, BodyType::NEUTRAL), 1);
+    assert_eq!(body_type_multiplier(BodyType::POSITIVE, BodyType::WEIRD), -1);
+}
+
+#[test]
+fn body_type_multiplier_negative() {
+    assert_eq!(body_type_multiplier(BodyType::NEGATIVE, BodyType::POSITIVE), 1);
+    assert_eq!(body_type_multiplier(BodyType::NEGATIVE, BodyType::NEGATIVE), -1);
+    assert_eq!(body_type_multiplier(BodyType::NEGATIVE, BodyType::NEUTRAL), 1);
+    assert_eq!(body_type_multiplier(BodyType::NEGATIVE, BodyType::WEIRD), -1);
+}
+
+
+#[test]
+fn body_type_multiplier_neutral() {
+    assert_eq!(body_type_multiplier(BodyType::NEUTRAL, BodyType::POSITIVE), 1);
+    assert_eq!(body_type_multiplier(BodyType::NEUTRAL, BodyType::NEGATIVE), 1);
+    assert_eq!(body_type_multiplier(BodyType::NEUTRAL, BodyType::NEUTRAL), 1);
+    assert_eq!(body_type_multiplier(BodyType::NEUTRAL, BodyType::WEIRD), -1);
+}
+
+#[test]
+fn body_type_multiplier_weird() {
+    assert_eq!(body_type_multiplier(BodyType::WEIRD, BodyType::POSITIVE), -1);
+    assert_eq!(body_type_multiplier(BodyType::WEIRD, BodyType::NEGATIVE), -1);
+    assert_eq!(body_type_multiplier(BodyType::WEIRD, BodyType::NEUTRAL), -1);
+    assert_eq!(body_type_multiplier(BodyType::WEIRD, BodyType::WEIRD), -1);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+mod body;
+
+use std::vec;
+
 use sfml::graphics::{RenderWindow, RenderTarget, CircleShape, Transformable, Shape, Color};
 use sfml::window::{ContextSettings, Style, Event};
 use sfml::system::Vector2f;
+use crate::body::*;
 
 fn main() {
     let mut window = RenderWindow::new(
@@ -12,9 +17,13 @@ fn main() {
 
     window.set_framerate_limit(60);
 
-    let mut circle = CircleShape::new(300f32, 10);
-    circle.set_position(Vector2f::new(100.0, 100.0));
-    circle.set_fill_color(Color::rgb(0,0,255));
+    let mut circle_vec = vec![CircleShape::new(10f32, 10), CircleShape::new(10f32, 10)];
+    let mut body_vec = vec![Body::new(Vector2f::new(0.0,0.0), 100.0, BodyType::NEUTRAL), Body::new(Vector2f::new(0.0,0.0), 100.0, BodyType::NEUTRAL)];
+
+    circle_vec[0].set_position(Vector2f::new(400.0, 100.0));
+    circle_vec[0].set_fill_color(Color::rgb(0,0,255));
+    circle_vec[1].set_position(Vector2f::new(200.0, 100.0));
+    circle_vec[1].set_fill_color(Color::rgb(255,0,0));
 
     while window.is_open() {
         while let Some(event) =  window.poll_event() {
@@ -23,8 +32,25 @@ fn main() {
             }
         }
         window.clear(Color::rgb(0,0,0));
-        circle.move_(Vector2f::new(1.0,1.0));
-        window.draw(&circle);
+
+        let mut i = 0;
+        while i < circle_vec.len() {
+            let mut j = i + 1;
+            while j < circle_vec.len() {
+                let direction_vector = Vector2f::new(circle_vec[j].position().x - circle_vec[i].position().x, circle_vec[j].position().y - circle_vec[i].position().y);
+                let distance = length_of_vector(direction_vector);
+                let normalized_direction_vector = normalize(direction_vector, distance);
+                let gravitational_force = calculate_force_of_gravity(body_vec[i].mass, body_vec[j].mass, distance);
+                let body_type_modifier = body_type_multiplier(body_vec[i].body_type, body_vec[j].body_type);
+                body_vec[i].velocity += normalized_direction_vector * gravitational_force * body_type_modifier as f32;
+                body_vec[j].velocity += normalized_direction_vector * gravitational_force * body_type_modifier as f32 * -1_f32; // negate this since force is opposite direction
+                j += 1;
+            }
+            circle_vec[i].move_(body_vec[i].velocity);
+            window.draw(&circle_vec[i]);
+            i += 1;
+        }
+
         window.display();
     }
 }
